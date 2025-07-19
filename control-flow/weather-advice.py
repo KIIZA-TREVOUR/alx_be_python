8bdcc571e014d2258de77e659113f12363be4566
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-07-19 09:47:43 +0300

    added weather-advice file

---
+++ control-flow/weather-advice.py
@@ -0,0 +1,9 @@
+input = input("What is the wheather like today? (sunny/rainy/cold): ").lower()
+if input == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif input == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif input == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
